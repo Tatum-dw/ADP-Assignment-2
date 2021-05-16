36ee70de0c5d710f8b0f377c2f3f21f1b9e67a49
Tatum-dw <69960567+Tatum-dw@users.noreply.github.com>
2021-05-16 17:01:47 +0200

    Add files via upload

--- Collections/src/main/java/za/ac/cput/Cars.java
+++ Collections/src/main/java/za/ac/cput/Cars.java
@@ -8,60 +8,71 @@ package za.ac.cput;
 
 import java.util.*;
 
-public class Cars implements Collections{
+public class Cars{
 
+    //1. Collection
+    Collection<String>yearModel = new LinkedList<String>(Arrays.asList("2012", "2016", "2008"));
 
-    //1. Map
+    //2. Map
     HashMap<String, String> licence = new HashMap<String, String>();
 
-    //2. Set
+    //3. Set
     String[] arr = {"Blue", "Black", "Silver"};
     TreeSet<String> colours = new TreeSet<String>(Arrays.asList(arr));
 
-    //3. List
+    //4. List
     ArrayList<String> cars = new ArrayList<String>(Arrays.asList("Toyota", "BMW", "Tesla"));
 
-    @Override
     public void add() {
-        //1. Map
+        //1. Collection
+        yearModel.add("2018");
+        System.out.println(yearModel);
+
+        //2. Map
         licence.put("Toyota", "CA1234567");
         System.out.println(licence);
 
-        //2. Set
+        //3. Set
         colours.add("White");
         System.out.println(colours);
 
-        //3. List
+        //4. List
         cars.add("Ford");
         System.out.println(cars);
     }
 
-    @Override
     public void remove() {
-        //1. Map
+        //1. Collection
+        yearModel.remove("2008");
+        System.out.println(yearModel);
+
+        //2. Map
         licence.remove("BMW");
         System.out.println(licence);
 
-        //2. Set
+        //3. Set
         colours.remove("Black");
         System.out.println(colours);
 
-        //3. List
+        //4. List
         cars.remove("Toyota");
         System.out.println(cars);
     }
 
-    @Override
     public void find() {
-        //1. Map
+        //1. Collection
+        yearModel.contains("2016");
+        System.out.println(yearModel);
+
+        //2. Map
         licence.containsKey("Ford");
         System.out.println(licence);
 
-        //2. Set
+        //3. Set
         colours.contains("Blue");
         System.out.println(colours);
 
-        //3. List
+        //4. List
         cars.contains("BMW");
         System.out.println(cars);
     }
--- Collections/src/test/java/za/ac/cput/CarsTest.java
+++ Collections/src/test/java/za/ac/cput/CarsTest.java
@@ -10,9 +10,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
-public class CarsTest implements CollectionsTest{
+public class CarsTest{
+    //1. Collection
+    Collection<String>yearModel = new LinkedList<String>(Arrays.asList("2012", "2016", "2008"));
 
-    //1. Map
+    //2. Map
     HashMap<String, String> licence = new HashMap<String, String>();
 
     public void setMap(HashMap<String, String>licence) {
@@ -21,64 +23,84 @@ public class CarsTest implements CollectionsTest{
         licence.put("Ford", "CA2483592");
     }
 
-    //2. Set
+    //3. Set
     String[] arr = {"Blue", "Black", "Silver"};
     TreeSet<String> colours = new TreeSet<String>(Arrays.asList(arr));
 
-    //3. List
+    //4. List
     ArrayList<String> cars = new ArrayList<String>(Arrays.asList("Toyota", "BMW", "Tesla"));
 
     @Test
     public void testAdd(){
-        //1. Map
+        System.out.println("All Add Tests:");
+
+        //1. Collection
+        System.out.println("1. COLLECTION: " + yearModel);
+        yearModel.add("2018");
+        System.out.println("Collection Added 2018 model: " + yearModel + "\n");
+
+        //2. Map
         setMap(licence);
-        System.out.println(licence);
+        System.out.println("2. MAP: " + licence);
         licence.put("Toyota", "CA1234567");
         System.out.println("Map Added Toyota: " + licence + "\n");
 
-        //2. Set
-        System.out.println(colours);
+        //3. Set
+        System.out.println("3. SET: " + colours);
         colours.add("White");
         System.out.println("Set Added colour White: " + colours + "\n");
 
-        //3. List
-        System.out.println(cars);
+        //4. List
+        System.out.println("4. LIST: " + cars);
         cars.add("Ford");
         System.out.println("List Added car Ford" + cars + "\n");
     }
 
     @Test
     public void testRemove(){
-        //1. Map
+        System.out.println("All Remove Tests:");
+
+        //1. Collection
+        System.out.println("1. COLLECTION: " + yearModel);
+        System.out.println("Collection Removed 2008 model: " + yearModel.remove("2008"));
+        System.out.println(yearModel + "\n");
+
+        //2. Map
         setMap(licence);
-        System.out.println(licence);
+        System.out.println("2. MAP: " + licence);
         System.out.println("HashMap Removed BMW: " + licence.remove("BMW"));
         System.out.println(licence + "\n");
 
-        //2. Set
-        System.out.println(colours);
+        //3. Set
+        System.out.println("3. SET: " + colours);
         System.out.println("Set Removed colour Black: " + colours.remove("Black"));
         System.out.println(colours + "\n");
 
-        //3. List
-        System.out.println(cars);
+        //4. List
+        System.out.println("4. LIST: " + cars);
         System.out.println("List Removed car Toyota: " + cars.remove("Toyota"));
         System.out.println(cars + "\n");
     }
 
     @Test
     public void testFind(){
-        //1. Map
+        System.out.println("All Find Tests:");
+
+        //1. Collection
+        System.out.println("1. COLLECTION: " + yearModel);
+        System.out.println("Collection finds 2016 model: " + yearModel.contains("2016") + "\n");
+
+        //2. Map
         setMap(licence);
-        System.out.println(licence);
+        System.out.println("2. MAP: " + licence);
         System.out.println("HashMap finds Ford: " + licence.containsKey("Ford") + "\n");
 
-        //2. Set
-        System.out.println(colours);
+        //3. Set
+        System.out.println("3. SET: " + colours);
         System.out.println("Set finds colour Blue: " + colours.contains("Blue") + "\n");
 
-        //3. List
-        System.out.println(cars);
+        //4. List
+        System.out.println("4. LIST: " + cars);
         System.out.println("List finds BMW: " + cars.contains("BMW") + "\n");
     }
 }
